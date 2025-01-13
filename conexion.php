63cfd138d921aeaa66e8ef5748f3808a90052a62
Neron6 <victorbarba56@gmail.com>
2025-01-12 22:55:08 -0600

    conexion.php

---
+++ conexion.php
@@ -0,0 +1,19 @@
+<?php
+//datos del usuario para la conexión a la BD
+//Se establecen variables de conexión
+
+$host = "localhost"; //servidor de la base de datos
+$usuario = "victor";
+$password = "SqTsb6Qj";
+$base_datos = "puntobazarv";
+
+//Se crea la conexión. Se establece la variable de la conexión
+$conexion = new mysqli($host, $usuario, $password, $base_datos);
+
+//Verificar la conexión con condicional
+if ($conexion->connect_error) {
+    die("Error de conexión: " . $conexion->connect_error);
+} else {
+    "Conexión exitosa.";
+}
+?>
