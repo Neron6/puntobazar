916d33e0132cc598d6569df33b3a862d1d7430af
Neron6 <victorbarba56@gmail.com>
2025-01-21 16:38:14 -0600

    registro.php

--- registro.php
+++ registro.php
@@ -1,29 +1,41 @@
 <!-- Formulario general-->
 <!DOCTYPE html>
 <html lang="es">
+<header>
+    <h1>Punto Bazar</h1>
+    <h2>Sistema de gestión</h2>
+</header>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registro</title>
-    <link rel="stylesheet" href="css/estilos.css">
+    <title>Registro de Usuarios</title>
+    <link rel="stylesheet" href="assets/css/registro.css">
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
 </head>
 <body>
+<div class="registro-container">
     <h2>Registro de Usuarios</h2>
-    <form action="registro.php" method="POST">
+    <form action="registro.php" method="POST" class="form-registro">
         <label for="nombre">Nombre:</label>
         <input type="text" name="nombre" placeholder="Nombre" id="nombre" required>
-
-        <label for="correo">Email (opcional):</label>
-        <input type="email" name="correo" placeholder="Email" id="correo">
 
         <label for="contrasena">Contraseña:</label>
         <input type="password" name="contrasena" placeholder="Contraseña" id="contrasena" required>
 
-        <button type="submit">Registrar</button>
+        <button type="submit" class="btn-registrar">Registrar</button>
     </form>
-    <a href="login.php">¿Ya tienes cuenta? Inicia Sesión</a>
+    <div class="link-login">
+        <a href="login.php">¿Ya tienes cuenta? Inicia Sesión</a>
+    </div>
+</div>
 </body>
+    <footer>
+        <h3>© 2025 Sistema de Gestión. Punto Bazar.</h3>
+    </footer>
 </html>
+
+
 
 <!-- Lógica de programación en PHP-->
 
